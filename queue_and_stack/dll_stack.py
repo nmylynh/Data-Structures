9f733ea6fafd0a981b5bb4e272682245e67aa1f3
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-09-21 14:40:48 -0700

    Update dll_stack.py

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,6 +2,8 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+# A DLL is a dynamic link library that contains code and data that can be used by more than one program at the same time. This is a shared library.
+# By using a DLL, a program can be modularized into separate components.
 class Stack:
   def __init__(self):
     self.size = 0
